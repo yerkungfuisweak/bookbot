46aed5212866652d933c71f2e0516820192acf66
yerkungfuisweak <yerkungfuisweak@gmail.com>
2024-12-21 21:03:38 +0000

    word count added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-if __name__ == "__main__":
+        words = file_contents.split()
+        print(len(words))
+if __name__ == "__main__":    
     main()
+

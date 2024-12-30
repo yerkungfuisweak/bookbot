470c1f8785ff56b5b3669daa6e893710dfb4f885
yerkungfuisweak <yerkungfuisweak@gmail.com>
2024-12-30 23:40:28 +0000

    count characters function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+        characters = {}
+        for char in file_contents.lower():
+            if char not in characters:
+                characters[char] = 1  # First time seeing this character
+            else:
+                characters[char] += 1  # Add one to the existing count
+    print(characters)
 if __name__ == "__main__":    
     main()
 
